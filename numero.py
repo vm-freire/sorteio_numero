2e0ca5947b897fbf840633069cdd3bb8a7b7982c
Vitor Freire <v.malicki.freire@gmail.com>
2025-02-06 10:51:07 -0300

    alterando limite para 100

--- numero.py
+++ numero.py
@@ -3,9 +3,9 @@ import random
 from math import trunc
 
 print('-='* 40)
-print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('Vou pensar em um número entre 0 e 100. Tente adivinhar...')
 
-aleatorio = trunc(random.random()*6)
+aleatorio = trunc(random.random()*101)
 
 #Linha de comando  para verificar funcionalidade
 #print(aleatorio)
