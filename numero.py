70d7a72130b84f3dde767a9ad2a6ce7fda4902b9
Vitor Freire <v.malicki.freire@gmail.com>
2025-02-06 10:46:52 -0300

    projeto inicial

---
+++ numero.py
@@ -0,0 +1,19 @@
+# Jogo de adivinhar número
+import random
+from math import trunc
+
+print('-='* 40)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+
+aleatorio = trunc(random.random()*6)
+
+#Linha de comando  para verificar funcionalidade
+#print(aleatorio)
+
+numero = int(input('Qual número estou pensando: '))
+
+
+if numero == aleatorio:
+    print('Parabéns, você acertou!! Eu estava pensando em {}.'.format(aleatorio))
+else:
+    print('Que pena tente novamente, estava pensando em {}'.format(aleatorio))
